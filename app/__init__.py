4b57ba0038c89f70429b774d66edf46e76229d2d
Samuel Williams (crypto) <samuelhwilliams+cryptop@gmail.com>
2017-02-26 19:38:17 +0000

    tweak rate limiter

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_limiter.util import get_remote_address
 from flask_cache import Cache
 
 app = Flask(__name__)
-limiter = Limiter(app, key_func=get_remote_address, global_limits=['10 per minute'])
+limiter = Limiter(app, key_func=get_remote_address, global_limits=['50 per minute'])
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 from app import views
