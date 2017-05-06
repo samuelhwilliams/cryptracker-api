dd1823cef140c5e4338ddea0808dc3db116817f1
Samuel Williams (crypto) <samuelhwilliams+cryptop@gmail.com>
2017-05-06 12:17:18 +0100

    reduce timeout on cache from 1 hour to 5 minutes for more responsive app

--- app/helpers.py
+++ app/helpers.py
@@ -5,10 +5,10 @@ from urllib import request
 
 from app import cache
 
-@cache.cached(timeout=3600, key_prefix="coinmarketcap_data")
+@cache.cached(timeout=300, key_prefix="coinmarketcap_data")
 def coinmarketcap_data():
     return json.loads(request.urlopen('https://api.coinmarketcap.com/v1/ticker/').read().decode('utf8'))
 
-@cache.cached(timeout=3600, key_prefix="fixerio_data")
+@cache.cached(timeout=300, key_prefix="fixerio_data")
 def fixerio_data():
     return json.loads(request.urlopen('https://api.fixer.io/latest?base=USD').read().decode('utf8'))
